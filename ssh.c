b82ad3eab68bf804af46f047e77458dfc49d4404
John Reach <john.reach@gmail.com>
2016-12-02 14:13:18 +0000

    add redirection with, >, <, >>

--- ssh.c
+++ ssh.c
@@ -3,12 +3,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <setjmp.h>
 #include <signal.h>
 #include <errno.h>
 
 #define MAXLINE	259
-#define PROMPT "> "
+#define PROMPT getPrompt()
 #define MAX_ARG_LIST	200
 
 extern char **environ;
@@ -27,6 +30,8 @@ void execExit(char *cmd[]);
 void execPwd(char *cmd[]);
 void execCd(char *cmd[]);
 
+int redirect_out(int new_fd);
+
 builtInCmd builtInCmds[] = {
 	{"exit", execExit  },
 	{"pwd",  execPwd   },
@@ -35,6 +40,7 @@ builtInCmd builtInCmds[] = {
 int builtInCnt = sizeof(builtInCmds)/sizeof(builtInCmd);
 int isBuiltIn(char *cmd);
 void execBuiltIn(int i, char *cmd[]);
+const char* getPrompt();
 
 // capture SIG_INT and recover
 sigjmp_buf ctrlc_buf;
@@ -46,8 +52,15 @@ int main(int argc, char *argv[]) {
 
 	char line[MAXLINE];
 	pid_t childPID;
-	int argn; char *args[MAX_ARG_LIST];
+	int argn; 
+	char *args[MAX_ARG_LIST];
 	int cmdn;
+	
+	int in_redirect_n  = 0;
+	int out_redirect_n = 0;
+	
+	// Boolean for append out
+	int append_out	   = 0;
 
 	// setup SIG_INT handler
 	if (signal(SIGINT, ctrl_hndlr) == SIG_ERR)
@@ -68,13 +81,84 @@ int main(int argc, char *argv[]) {
 			line[strlen(line)-1] = '\0';
 		// build argument list
 		args[argn=0]=strtok(line, " \t");
-		while(args[argn]!=NULL && argn<MAX_ARG_LIST)
+		while(args[argn]!=NULL && argn<MAX_ARG_LIST) {
+			
 			args[++argn]=strtok(NULL," \t");
+			
+			
+			// handle redirection
+			if(args[argn] != NULL && strcmp(args[argn], ">") == 0 )
+				out_redirect_n = argn+1;
+				
+			if(args[argn] != NULL && strcmp(args[argn], "<") == 0)
+				in_redirect_n = argn+1;
+				
+			if(args[argn] != NULL && strcmp(args[argn], ">>") == 0) {
+				
+				out_redirect_n = argn+1;
+				append_out = 1;
+			}
+				
+			
+		}
 
+		
 	// execute commandline
-	if ((cmdn = isBuiltIn(args[0]))>-1) {
+	if (args[0] != NULL && (cmdn = isBuiltIn(args[0]))>-1) {
+		
+		int stdout_copy = dup(1);
+		int stdin_copy  = dup(0);
+		
+		// Redirct stdin or exit upon fail to open file
+		if(in_redirect_n > 0 && args[in_redirect_n] != NULL) {
+		
+			char* f_in_string = malloc(1024);
+			size_t buffer = 80;
+			strcpy(f_in_string, args[in_redirect_n]);
+			
+			FILE* f_in = fopen(f_in_string, "r");
+			
+			if(f_in != NULL) {
+				
+				getline(&f_in_string, &buffer, f_in);
+				args[in_redirect_n] = strtok(f_in_string, "\n");
+
+				args[in_redirect_n-1] = args[in_redirect_n];
+				args[in_redirect_n] = NULL;
+				
+			} else {
+				
+				fprintf(stderr, "ERROR: %s: %s\n", args[in_redirect_n], 
+					strerror(errno));
+					
+				_exit(1);
+			}
+			
+			fclose(f_in);
+			free(f_in_string);
+			
+		}
+		
+		
+		// Redirect stdout to
+		if(out_redirect_n > 0 ) {
+		
+			// Append or not	
+			if(append_out == 0) 
+				redirect_out(open(args[out_redirect_n], O_WRONLY|O_CREAT, 0666));
+			else
+				redirect_out(open(args[out_redirect_n], O_WRONLY|O_CREAT|O_APPEND, 0666));
+		}
+		
+		// Execute the command
 		execBuiltIn(cmdn, args);
+		
+		// Reset stdout and stdin
+		redirect_out(stdout_copy);
+		//redirect_in(stdin_copy);
+
 	} else {
+		
 		childPID = fork();
 		if (childPID == 0) {
 			execve(args[0], args, environ);
@@ -117,7 +201,7 @@ void execExit(char *cmd[]) {
 }
 
 void execPwd(char *cmd[]) {
-	char cwd[2048];
+	char cwd[1024];
 	
 	getcwd(cwd, sizeof(cwd));
 	
@@ -129,10 +213,56 @@ void execPwd(char *cmd[]) {
 }
 
 void execCd(char *cmd[]) {
-
+	
+	/*
+	// Accept home '~' shortcut
+	if(cmd[1] != NULL && strcmp(cmd[1],'~') == 0 || strcmp(cmd[1],'~/') == 0) {
+		strcpy(cmd[1], "/home/")
+		strcat(cmd[1], getlogin());
+	}
+	*/
+	/*
+	if(strcmp(cmd[1], "<") == 0 && cmd[2] != NULL)
+		cmd[1] = cmd[2];
+	*/
+	
 	if(chdir(cmd[1]) < 0) {
 	
 		fprintf(stderr, "ERROR: %s: %s\n", cmd[1], strerror(errno));
 		
 	}
+}
+
+const char* getPrompt() {
+	char* prompt = malloc(1024);
+	
+	if(getcwd(prompt, sizeof(prompt)) == NULL)
+		strcpy(prompt, "~ $ ");
+	else
+		strcat(prompt, " $ ");
+	
+	free(prompt);
+	
+	return prompt;
+}
+
+int redirect_out(int new_fd) {
+
+	if(new_fd >= 0) {
+		
+		dup2(new_fd, STDOUT_FILENO);
+		close(new_fd);
+	}
+	
+	return new_fd;
+}
+
+int redirect_in(int new_fd) {
+
+	if(new_fd < 0) {
+		dup2(new_fd, STDIN_FILENO);
+		close(new_fd);
+	}
+	
+	return new_fd;
 }
